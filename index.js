5ff3f07e90fe006f42645645cb69c6540c6c209c
Katy Carr <khcarr9@gmail.com>
2017-10-27 13:37:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  window.gameInterval = null;
+  clearInterval(gameInterval);
   var toRemove = document.body.getElementsByClassName('rock')
   for(let i=0;i<ROCKS.length;i++) {
     toRemove = ROCKS[i];
