a2a1d889c92a90bec9421bc2c99891a8ba92a118
Katy Carr <khcarr9@gmail.com>
2017-10-27 13:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ function moveDodgerRight() {
   var leftNumbers = DODGER.style.left.replace('px','');
   var left = parseInt(leftNumbers,10);
   if(left<360) {
-    
+
     DODGER.style.left = `${left+4}px`;
     // implement me!
     /**
